6cc299703159c00edc927bed0ad6fa8c78738890
Orkhan Mamedov <orx.mamedov@gmail.com>
2024-05-01 10:58:55 +0400

    deleted utils

--- src/main/java/com/orkhanmamedov/expressbank/util/enums/ConfirmationMethod.java
+++
@@ -1,5 +0,0 @@
-package com.orkhanmamedov.expressbank.util.enums;
-
-public enum ConfirmationMethod {
-  EMAIL
-}
--- src/main/java/com/orkhanmamedov/expressbank/util/enums/Operation.java
+++
@@ -1,7 +0,0 @@
-package com.orkhanmamedov.expressbank.util.enums;
-
-public enum Operation {
-  OPEN_DEPOSIT,
-  CREATE_PAYMENT,
-  CREATE_TRANSFER
-}
